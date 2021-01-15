0da8adfb05a0960d0809817b0707e893724f7a57
petarvlajics3219 <vlajic.p27@gmail.com>
2021-01-16 00:21:49 +0100

    End Commit

--- server.js
+++ server.js
@@ -1,14 +1,74 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const cookieParser = require('cookie-parser')
+const fileupload = require('express-fileupload')
+const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
+const auth = require('./routes/auth')
+const users = require('./routes/users')
+const reviews = require('./routes/review')
 // Load env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' })
 
+//connect to database
+connectDB()
 
-const app = express();
+const app = express()
 
+//Body parser
+app.use(express.json())
 
-const PORT = process.env.PORT || 5000;
+// Cookie parser
+app.use(cookieParser())
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+//Dev logging middleware
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// File uploading
+app.use(fileupload())
+
+// Sanitize data
+app.use(mongoSanitize())
+
+// Set securty headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
+app.use('/api/v1/reviews', reviews)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+const servert = app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}.red`)
+  //Close server & exit process
+  servert.close(() => process.exit(1))
+})
